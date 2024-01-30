75633c75cf7829d9c212777668fd0503f9e8ed61
구환준/모건 <120353547+koojun99@users.noreply.github.com>
2024-01-30 20:50:26 +0900

    Title: 아름다운 수, Time: 119ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240130/아름다운 수/beautiful-number.py
+++ 240130/아름다운 수/beautiful-number.py
@@ -1,12 +1,27 @@
+def is_beautiful(arr):
+    i = 0
+    while i < len(arr):
+        num = arr[i]
+        count = 0
+        while i < len(arr) and arr[i] == num:
+            count += 1
+            i += 1
+        if count != num:
+            return False
+    return True
+
+def count_beautiful_numbers(n, arr=[], count=0):
+    if len(arr) == n:
+        if is_beautiful(arr):
+            return 1
+        else:
+            return 0
+    total = 0
+    for i in range(1, 5):
+        arr.append(i)
+        total += count_beautiful_numbers(n, arr, count)
+        arr.pop()
+    return total
+
 n = int(input())
-
-def count_beautiful_numbers(n, start=1):
-    if n == 0:
-        return 1
-    count = 0
-    for i in range(start, 5):
-        if n >= i:
-            count += count_beautiful_numbers(n - i, i)
-    return count
-
 print(count_beautiful_numbers(n))
