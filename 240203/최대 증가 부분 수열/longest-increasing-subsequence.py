1cdeb3f686defa80155182023ed2d3f865c0a17d
구환준/모건 <120353547+koojun99@users.noreply.github.com>
2024-02-03 09:40:00 +0900

    Title: 최대 증가 부분 수열, Time: 158ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240203/최대 증가 부분 수열/longest-increasing-subsequence.py
@@ -0,0 +1,17 @@
+def lis_length(nums):
+    N = len(nums)
+    dp = [1] * N  # 모든 위치에서 LIS의 초기 길이는 1입니다.
+    
+    for i in range(N):
+        for j in range(i):
+            if nums[i] > nums[j]:
+                dp[i] = max(dp[i], dp[j] + 1)
+    
+    return max(dp)
+
+# 입력 예제
+N = int(input())
+nums = list(map(int, input().split()))
+
+# 출력
+print(lis_length(nums))
