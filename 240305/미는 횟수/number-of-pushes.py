6c578feb797cb3c8f5a325f236e3a45e056ed755
구환준/모건 <120353547+koojun99@users.noreply.github.com>
2024-03-05 23:04:08 +0900

    Title: 미는 횟수, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

--- 240305/미는 횟수/number-of-pushes.py
+++ 240305/미는 횟수/number-of-pushes.py
@@ -1,12 +1,15 @@
 stringA = input()
 stringB = input()
 n = 0
+found = False  # 문자열이 일치하는지 여부를 추적하는 플래그
 
-while stringA != stringB:
-    if n == len(stringA)+1:
-        print(-1)
-        break
+for _ in range(len(stringA)):  # 최대 stringA의 길이만큼만 회전
     stringA = stringA[-1] + stringA[:-1]
     n += 1
+    if stringA == stringB:
+        print(n)
+        found = True
+        break
 
-print(n)
+if not found:  # 일치하는 문자열을 찾지 못한 경우
+    print(-1)
