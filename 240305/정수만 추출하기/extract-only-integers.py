47908f420af5bb2d5ab15d387c573613d05642f2
구환준/모건 <120353547+koojun99@users.noreply.github.com>
2024-03-05 22:11:14 +0900

    Title: 정수만 추출하기, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240305/정수만 추출하기/extract-only-integers.py
+++ 240305/정수만 추출하기/extract-only-integers.py
@@ -1,12 +1,17 @@
 a, b = input().split()
-
+numA = 0
+numB = 0
 for i in range(len(a)):
     if not a[i].isdigit():
         numA = int(a[:i])
         break
+else:
+    numA = int(a)
 for i in range(len(b)):
     if not b[i].isdigit():
         numB = int(b[:i])
         break
+else:
+    numB = int(b)
 
 print(numA + numB)
