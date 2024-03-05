9c27ef24a3b8f41039c29b9e6dbc3ba39b1ded4a
구환준/모건 <120353547+koojun99@users.noreply.github.com>
2024-03-05 15:37:41 +0900

    Title: 문자 제거하기, Time: 146ms, Memory: 24MB, Status: Passed - CodeTree

--- 240305/문자 제거하기/del-char.py
+++ 240305/문자 제거하기/del-char.py
@@ -2,7 +2,7 @@ word = input()
 toList = list(word)
 for _ in range(len(word) - 1):
     toDel = int(input())
-    if toDel > len(toList):
+    if toDel >= len(toList):
         toDel = -1
     toList.pop(toDel)
     print(''.join(toList))
