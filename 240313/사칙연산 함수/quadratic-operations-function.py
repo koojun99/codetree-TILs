20ec76c0cb90efee5a7650c0ad812de1daec7c04
구환준/모건 <120353547+koojun99@users.noreply.github.com>
2024-03-13 14:40:01 +0900

    Title: 사칙연산 함수, Time: 148ms, Memory: 24MB, Status: Passed - CodeTree

--- 240313/사칙연산 함수/quadratic-operations-function.py
+++ 240313/사칙연산 함수/quadratic-operations-function.py
@@ -15,6 +15,6 @@ def operation(a, o, c):
     else:
         return False
 if not operation(a,o,c):
-    operation(a, o, c)
+    print("False")
 else:
     print(a + " " + o + " " + c + " = " + str(operation(a,o,c)))
