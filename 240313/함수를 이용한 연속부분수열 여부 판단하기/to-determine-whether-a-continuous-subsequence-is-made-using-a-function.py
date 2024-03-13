3d268f3442455a3a67ba09aeed605cf76a71494b
구환준/모건 <120353547+koojun99@users.noreply.github.com>
2024-03-13 20:10:21 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 98ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240313/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
+++ 240313/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -1,14 +1,18 @@
 n1, n2 = map(int, input().split())
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
-
+isValid = False
 def is_partial(a, b, pos):
     if a[pos:len(b)+1] == b:
-        print("Yes")
-    else:
-        print("No")
+        return True
+    return False
 
 for i in range(n1):
-    if a[i] == b[0]:
-        is_partial(a, b, i)
+    if a[i] == b[0] and is_partial(a, b, i):
+        isValid = True
         break
+
+if isValid:
+    print("Yes")
+else:
+    print("No")
