0cb62df1c42c0c7f4b6686d9e3a5680379f8a9ac
구환준/모건 <120353547+koojun99@users.noreply.github.com>
2024-03-13 14:39:10 +0900

    Title: 사칙연산 함수, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240313/사칙연산 함수/quadratic-operations-function.py
@@ -0,0 +1,19 @@
+a, o, c = input().split()
+
+
+def operation(a, o, c):
+    a = int(a)
+    c = int(c)
+    if o == "+":
+        return a + c
+    elif o == "-":
+        return a - c
+    elif o == "/":
+        return a // c
+    elif o == "*":
+        return a * c
+    else:
+        return False
+if not operation(a,o,c):
+    operation(a, o, c)
+print(a + " " + o + " " + c + " = " + str(operation(a,o,c)))
