be5a840b5f15b4c82dc9d28ae80833d66e057a20
구환준/모건 <120353547+koojun99@users.noreply.github.com>
2024-03-13 20:32:45 +0900

    Title: 그 계절, 그 날, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240313/그 계절, 그 날/that-season-that-day.py
@@ -0,0 +1,45 @@
+def season(month):
+    if 1 <= month <=2 or month == 12:
+        print("Winter")
+    if 3 <= month <= 5:
+        print("Spring")
+    if 6 <= month <= 8:
+        print("Summer")
+    if 9 <= month <= 11:
+        print("Fall")
+
+def isYoon(year):
+    if year % 4 == 0 and year % 100 != 0:
+        return True
+    if year % 4 == 0 and year % 100 == 0:
+        if year % 400 == 0:
+            return True
+    if year % 4 == 0:
+        return True
+    return False
+
+def isValid(year, month, day):
+    thirty = [4, 6, 9, 11]
+        
+    if month == 2:
+        if isYoon(year):
+            if day > 29:
+                return False
+            else:
+                return True
+        if day > 28:
+            return False
+    if month in thirty:
+        if day > 30:
+            return False
+    else:
+        if day > 31:
+            return False
+    return True
+
+y, m, d = map(int, input().split())
+
+if isValid(y, m, d):
+    season(m)
+else:
+    print(-1)
