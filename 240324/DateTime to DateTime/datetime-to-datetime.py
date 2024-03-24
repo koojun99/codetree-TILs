be04fd76503ab95a3a93f97dc6037e17904d6042
구환준/모건 <120353547+koojun99@users.noreply.github.com>
2024-03-24 23:02:13 +0900

    Title: DateTime to DateTime, Time: 5040ms, Memory: 24MB, Status: Time Limit Exceed - CodeTree

--- 240324/DateTime to DateTime/datetime-to-datetime.py
+++ 240324/DateTime to DateTime/datetime-to-datetime.py
@@ -1,18 +1,19 @@
 a, b, c = map(int, input().split())
 day, hour, mins = 11, 11, 11
 total = 0
-if a < 11 or b < 11 or c < 11:
+if a < 11 or (hour*60 + mins) < 671:
     print(-1)
-while (day, hour, mins) != (a, b, c):
-    total += 1
-    mins += 1
+else:
+    while (day, hour, mins) != (a, b, c):
+        total += 1
+        mins += 1
 
-    if mins == 60:
-        hour += 1
-        mins = 0
+        if mins == 60:
+            hour += 1
+            mins = 0
     
-    if hour == 24:
-        day += 1
-        hour = 0
+        if hour == 24:
+            day += 1
+            hour = 0
 
-print(total)
+    print(total)
