b61142f2219d681da993dcae01983abc23b6affa
구환준/모건 <120353547+koojun99@users.noreply.github.com>
2024-03-25 12:16:41 +0900

    Title: 그 요일은, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240325/그 요일은/the-day-of-the-day.py
+++ 240325/그 요일은/the-day-of-the-day.py
@@ -4,7 +4,7 @@ day_of_week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 count = 0
 day = 0
 def total_days(m,d):
-    days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     total = 0
     for i in range(1, m):
         total += days[i]
@@ -13,9 +13,9 @@ def total_days(m,d):
 
 diff = total_days(m1, d1) - total_days(m2, d2)
 diff = abs(diff)
-while day != diff+1:
-    day += 1
-    weekday = day_of_week[day%7]
-    if weekday == target:
+target_day_index = day_of_week.index(target)
+
+for i in range(diff + 1):
+    if (day + i) % 7 == target_day_index:
         count += 1
 print(count)
