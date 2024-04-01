7c3ef9d92edabd28ab5420cd5c53e3d13bab81db
구환준/모건 <120353547+koojun99@users.noreply.github.com>
2024-04-01 11:43:03 +0900

    Title: 연속되는 수 4, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240401/연속되는 수 4/continuous-number4.py
+++ 240401/연속되는 수 4/continuous-number4.py
@@ -9,6 +9,8 @@ for _ in range(n):
 for i in range(len(nums)):
     if i > 0 and nums[i] > nums[i-1]:
         size += 1
+    else:
+        size = 1
     max_size = max(max_size, size)
 
 print(max_size)
