7aa2ca3f90e249d741bd19a0c220056ef00c8df7
구환준/모건 <120353547+koojun99@users.noreply.github.com>
2024-04-02 14:01:27 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240402/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -0,0 +1,21 @@
+n, k, p, t = map(int, input().split())
+people = [0]*n
+count = [0]*n
+people[p-1] = 1
+events = []
+
+for _ in range(t):
+    time, a, b = map(int, input().split())
+    events.append((time, a, b))
+
+events.sort(key=lambda x: x[0])
+
+for time, a, b in events:
+    if (people[a-1] == 1 and count[a-1] < k)  or (people[b-1] == 1 and count[b-1] < k):
+        people[a-1] = 1
+        people[b-1] = 1
+        count[a-1] += 1
+        count[b-1] += 1
+
+for elem in people:
+    print(elem, end="")
