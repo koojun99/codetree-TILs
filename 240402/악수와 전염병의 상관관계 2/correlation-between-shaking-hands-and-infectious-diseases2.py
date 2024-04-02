09be48c50d09895a88dcc684f1702b2cd888bfea
구환준/모건 <120353547+koojun99@users.noreply.github.com>
2024-04-02 14:20:10 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240402/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 240402/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -1,27 +1,36 @@
-n, k, p, t = map(int, input().split())
-people = [0]*n
-count = [0]*n
-people[p-1] = 1
-events = []
+N, K, P, T = map(int, input().split())
 
-for _ in range(t):
-    time, a, b = map(int, input().split())
-    events.append((time, a, b))
+# 전염 상태와 전염 가능한 악수 횟수를 기록
+infected = {P: K}
+transmission_count = {i: 0 for i in range(1, N+1)}  # 각 개발자별 전파 횟수 기록
 
-events.sort()
+handshakes = []
+for _ in range(T):
+    t, x, y = map(int, input().split())
+    handshakes.append((t, x, y))
 
-for time, a, b in events:
-    if people[a-1] == 1 or people[b-1] == 1:
-        if people[a-1] == 1 and people[b-1] == 1:
-            count[a-1] = min(k, count[a-1]+1)
-            count[b-1] += min(k, count[b-1]+1)
-            
-        if people[a-1] == 1 and count[a-1] < k:
-            people[b-1] = 1
-            count[a-1] = min(k, count[a-1]+1)
-        if people[b-1] == 1 and count[b-1] < k:
-            people[a-1] = 1
-            count[b-1] += min(k, count[b-1]+1)
+handshakes.sort()  # 악수 정보를 시간 순으로 정렬
 
-for elem in people:
-    print(elem, end="")
+for _, x, y in handshakes:
+    # 감염 상태 업데이트
+    if x in infected and infected[x] > 0:
+        infected[y] = K
+    if y in infected and infected[y] > 0:
+        infected[x] = K
+    
+    # 악수를 통한 전파 횟수 업데이트
+    if x in infected:
+        transmission_count[x] += 1
+        if transmission_count[x] >= K:
+            infected[x] = 0  # 더 이상 전파할 수 없음
+    if y in infected:
+        transmission_count[y] += 1
+        if transmission_count[y] >= K:
+            infected[y] = 0  # 더 이상 전파할 수 없음
+
+# 최종 감염 상태 출력
+for i in range(1, N + 1):
+    if i in infected:
+        print(1, end="")
+    else:
+        print(0, end="")
