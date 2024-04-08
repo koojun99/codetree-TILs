d6885f0016e0d4d4d75e4a5f8d5d41873832d549
구환준/모건 <120353547+koojun99@users.noreply.github.com>
2024-04-08 23:26:27 +0900

    Title: 격자 위의 편안한 상태, Time: 188ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 240408/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
@@ -0,0 +1,25 @@
+dxs, dys = [-1, 0, 1, 0], [0, -1, 0, 1]
+n, m = map(int, input().split())
+grid = [[0]*n for _ in range(n)]
+
+def isValid(x, y):
+    return 0 <= x < n and 0 <= y < n
+
+def isComfort(x, y):
+    count = 0
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x + dx, y + dy
+        if isValid(nx, ny) and grid[nx][ny] == 1:
+            count += 1
+    if count >= 3:
+        return True
+    else:
+        return False
+
+for _ in range(m):
+    x, y = map(int, input().split())
+    grid[x-1][y-1] = 1
+    if isComfort(x-1, y-1):
+        print(1)
+    else:
+        print(0)
