991cbab66de93108374b74a8d3951e3dd598cea3
구환준/모건 <120353547+koojun99@users.noreply.github.com>
2024-04-11 19:36:00 +0900

    Title: 정수 선언하고 뺄셈, Time: 87ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240411/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = 97, b = 13;
+        System.out.println(a + " - " + b + " = " + (a - b));
+    }
+}
