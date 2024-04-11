dcb5434425720151dc05c969f324164486348b03
구환준/모건 <120353547+koojun99@users.noreply.github.com>
2024-04-11 19:39:53 +0900

    Title: 세 정수형 변수 선언, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240411/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = 7, b = 23, c = 30;
+        System.out.printf("%d + %d = %d", a, b, c);
+    }
+}
