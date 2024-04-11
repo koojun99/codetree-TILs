5be02e52c0588641d9722399e49ac89a8bfa1743
구환준/모건 <120353547+koojun99@users.noreply.github.com>
2024-04-11 19:42:40 +0900

    Title: 변수 출력하기 3, Time: 96ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240411/변수 출력하기 3/outputing-variables-3.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = 1, b = 2;
+        char c = 'C';
+        System.out.printf("%d->%d->%c", a, b, c);
+    }
+}
