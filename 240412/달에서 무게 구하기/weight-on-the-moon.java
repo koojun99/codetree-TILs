fa002720cd74219b2ed01337d770a04885b190ca
구환준/모건 <120353547+koojun99@users.noreply.github.com>
2024-04-12 11:44:40 +0900

    Title: 달에서 무게 구하기, Time: 93ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240412/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = 13;
+        double b = 0.165000;
+        System.out.printf("%d * %.6f = %.6f", a, b, (a*b));
+    }
+}
